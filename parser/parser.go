85f274c0b16ef6d57ffa1c71d7f206e77a806335
Daniel Silva <daniel@silvanolte.com>
2017-02-19 12:37:32 -0500

    Updated parser package. Made tests stronger, added new function, refactored where the cards are being declared.

--- parser/parser.go
+++ parser/parser.go
@@ -6,17 +6,33 @@ import (
 	"strings"
 )
 
-func ParseHand(hand string) ([]string, error) {
-	cards := [52]string{"2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "TH", "JH",
-		"QH", "KH", "AH", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "TS", "JS",
-		"QS", "KS", "AS", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "TC", "JC",
-		"QC", "KC", "AC", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "TD", "JD",
-		"QD", "KD", "AD"}
+var cards [52]string = [52]string{
+	"2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "TH", "JH", "QH", "KH", "AH", "2S",
+	"3S", "4S", "5S", "6S", "7S", "8S", "9S", "TS", "JS", "QS", "KS", "AS", "2C", "3C",
+	"4C", "5C", "6C", "7C", "8C", "9C", "TC", "JC", "QC", "KC", "AC", "2D", "3D", "4D",
+	"5D", "6D", "7D", "8D", "9D", "TD", "JD", "QD", "KD", "AD",
+}
 
+var valueConversion map[string]int = map[string]int{
+	"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8,
+	"9": 9, "T": 10, "J": 11, "Q": 12, "K": 13, "A": 14,
+}
+
+func ParseCardValue(s string) (int, error) {
+	value := valueConversion[s]
+
+	if value == 0 {
+		return value, fmt.Errorf("Invalid card value: Must be single digit 0-9 or one of T (10), J (Jack), Q (Queen), K (King), or A (Ace)")
+	} else {
+		return value, nil
+	}
+}
+
+func ParseHand(hand string) ([]string, error) {
 	parsedHand := strings.Split(hand, " ")
 
 	if len(parsedHand) != 5 {
-		return parsedHand, errors.New("Invalid hand: must have five cards")
+		return []string{""}, errors.New("Invalid hand: must have five cards")
 	}
 
 	for _, card := range parsedHand {
@@ -29,7 +45,7 @@ func ParseHand(hand string) ([]string, error) {
 			}
 		}
 		if !isCardValid {
-			return parsedHand, fmt.Errorf("Invalid hand: contains invalid card (%s)", card)
+			return []string{""}, fmt.Errorf("Invalid hand: contains invalid card")
 		}
 	}
 
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -4,67 +4,141 @@ import (
 	"testing"
 )
 
-func TestParseInvalidHandMissingCards(t *testing.T) {
+func TestParseCardValueInvalidCard(t *testing.T) {
 	var tests = []struct {
-		hand, expectedErr string
+		value               string
+		expectedParsedValue int
+		expectedErr         string
 	}{
-		{"", "Invalid hand: must have five cards"},
-		{" ", "Invalid hand: must have five cards"},
-		{"4S", "Invalid hand: must have five cards"},
-		{"4S 4C", "Invalid hand: must have five cards"},
-		{"4S 4C 2D", "Invalid hand: must have five cards"},
-		{"4S 4C 2D 4H", "Invalid hand: must have five cards"},
-		{"2H 4S 4C 2D 4H 2S", "Invalid hand: must have five cards"},
-		{"not a hand", "Invalid hand: must have five cards"},
-		{"not", "Invalid hand: must have five cards"},
+		{"0", 0, "Invalid card value: Must be single digit 0-9 or one of T (10), J (Jack), Q (Queen), K (King), or A (Ace)"},
+		{"NaN", 0, "Invalid card value: Must be single digit 0-9 or one of T (10), J (Jack), Q (Queen), K (King), or A (Ace)"},
+		{"2 3", 0, "Invalid card value: Must be single digit 0-9 or one of T (10), J (Jack), Q (Queen), K (King), or A (Ace)"},
+		{"23", 0, "Invalid card value: Must be single digit 0-9 or one of T (10), J (Jack), Q (Queen), K (King), or A (Ace)"},
 	}
 
 	for _, test := range tests {
-		_, err := ParseHand(test.hand)
+		parsedValue, err := ParseCardValue(test.value)
 		if err == nil {
-			t.Errorf("ParseHand(%q) didn't return a error where it was expected", test.hand)
+			t.Errorf("ParseCardValue(%q) err == nil but expected %q", test.value, err)
+		} else {
+			if err.Error() != test.expectedErr {
+				t.Errorf("ParseCardValue(%q) err == %q but expected %q", test.value, err, test.expectedErr)
+			}
+		}
+		if parsedValue != test.expectedParsedValue {
+			t.Errorf("ParseCardValue(%q) parsedValue == %q but expected %q",
+				test.value, parsedValue, test.expectedParsedValue)
+		}
+	}
+}
+
+func TestParseCardValueValidCard(t *testing.T) {
+	var tests = []struct {
+		value               string
+		expectedParsedValue int
+	}{
+		{"2", 2},
+		{"3", 3},
+		{"4", 4},
+		{"5", 5},
+		{"6", 6},
+		{"7", 7},
+		{"8", 8},
+		{"9", 9},
+		{"T", 10},
+		{"J", 11},
+		{"Q", 12},
+		{"K", 13},
+		{"A", 14},
+	}
+
+	for _, test := range tests {
+		parsedValue, err := ParseCardValue(test.value)
+		if err != nil {
+			t.Errorf("ParseHand(%q) err == %q but expected nil", test.value, err)
+		}
+		if parsedValue != test.expectedParsedValue {
+			t.Errorf("ParseCardValue(%q) == %t but expected %t",
+				test.value, parsedValue, test.expectedParsedValue)
+		}
+	}
+}
+
+func TestParseHandInvalidHandMissingCards(t *testing.T) {
+	var tests = []struct {
+		hand               string
+		expectedParsedHand []string
+		expectedErr        string
+	}{
+		{"", []string{""}, "Invalid hand: must have five cards"},
+		{" ", []string{""}, "Invalid hand: must have five cards"},
+		{"4S", []string{""}, "Invalid hand: must have five cards"},
+		{"4S 4C", []string{""}, "Invalid hand: must have five cards"},
+		{"4S 4C 2D", []string{""}, "Invalid hand: must have five cards"},
+		{"4S 4C 2D 4H", []string{""}, "Invalid hand: must have five cards"},
+		{"2H 4S 4C 2D 4H 2S", []string{""}, "Invalid hand: must have five cards"},
+		{"not a hand", []string{""}, "Invalid hand: must have five cards"},
+		{"not", []string{""}, "Invalid hand: must have five cards"},
+	}
+
+	for _, test := range tests {
+		parsedHand, err := ParseHand(test.hand)
+		if err == nil {
+			t.Errorf("ParseCardValue(%q) err == nil but expected %q", test.hand, err)
 		} else {
 			if err.Error() != test.expectedErr {
 				t.Errorf("ParseHand(%q) err == %q but expected %q", test.hand, err, test.expectedErr)
 			}
 		}
+		if parsedHand[0] != test.expectedParsedHand[0] ||
+			len(parsedHand) != len(test.expectedParsedHand) {
+			t.Errorf("ParseHand(%q) == %q but expected %q",
+				test.hand, parsedHand, test.expectedParsedHand)
+		}
 	}
 }
 
-func TestParseInvalidHandInvalidCards(t *testing.T) {
+func TestParseHandInvalidHandInvalidCard(t *testing.T) {
 	var tests = []struct {
-		hand, expectedErr string
+		hand               string
+		expectedParsedHand []string
+		expectedErr        string
 	}{
-		{"2H 4S 4C 2D 4P", "Invalid hand: contains invalid card (4P)"},
-		{"5T 4S 4C 2D 4P", "Invalid hand: contains invalid card (5T)"},
-		{"2H 4S 4C 2D 10H", "Invalid hand: contains invalid card (10H)"},
-		{"Every good boy does fine", "Invalid hand: contains invalid card (Every)"},
+		{"2H 4S 4C 2D 4P", []string{""}, "Invalid hand: contains invalid card"},
+		{"5T 4S 4C 2D 4P", []string{""}, "Invalid hand: contains invalid card"},
+		{"2H 4S 4C 2D 10H", []string{""}, "Invalid hand: contains invalid card"},
+		{"Every good boy does fine", []string{""}, "Invalid hand: contains invalid card"},
 	}
 
 	for _, test := range tests {
-		_, err := ParseHand(test.hand)
+		parsedHand, err := ParseHand(test.hand)
 		if err == nil {
-			t.Errorf("ParseHand(%q) didn't return a error where it was expected", test.hand)
+			t.Errorf("ParseCardValue(%q) err == nil but expected %q", test.hand, err)
 		} else {
 			if err.Error() != test.expectedErr {
 				t.Errorf("ParseHand(%q) err == %q but expected %q", test.hand, err, test.expectedErr)
 			}
 		}
+		if parsedHand[0] != test.expectedParsedHand[0] ||
+			len(parsedHand) != len(test.expectedParsedHand) {
+			t.Errorf("ParseHand(%q) == %q but expected %q",
+				test.hand, parsedHand, test.expectedParsedHand)
+		}
 	}
 }
 
-func TestParseValidHand(t *testing.T) {
-	input := "2H 4S 4C 2D 4H"
-	parsedHand, err := ParseHand(input)
+func TestParseHandValidHand(t *testing.T) {
+	hand := "2H 4S 4C 2D 4H"
+	parsedHand, err := ParseHand(hand)
 
 	if err != nil {
-		t.Errorf("ParseHand(%q) err == %q but expected nil", input, err)
+		t.Errorf("ParseHand(%q) err == %q but expected nil", hand, err)
 	}
 
 	handLen := len(parsedHand)
 	expectedHandLen := 5
 	if handLen != expectedHandLen {
-		t.Errorf("len(ParseHand(%q)) == %d but expected %d", input, handLen, expectedHandLen)
+		t.Errorf("len(ParseHand(%q)) == %d but expected %d", hand, handLen, expectedHandLen)
 	}
 
 	expectedCards := []string{"2H", "4S", "4C", "2D", "4H"}
