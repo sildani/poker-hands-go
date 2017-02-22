1b5196f0acd3acad8d9bd5d99281ae75fd05ea47
Daniel Silva <daniel@silvanolte.com>
2017-02-21 23:16:10 -0500

    Formatted main program

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-"fmt"
- "github.com/sildani/poker-hands-go/parser"
+	"fmt"
+	"github.com/sildani/poker-hands-go/parser"
 )
 
 func main() {
-  // hand := "AD AH QS JS TC"
-  hand := "AD AD QS JS TC"
-  parsedHand, _ := parser.ParseHand(hand)
-  fmt.Printf("parsedHand: %v\n", parsedHand)
+	// hand := "AD AH QS JS TC"
+	hand := "AD AD QS JS TC"
+	parsedHand, _ := parser.ParseHand(hand)
+	fmt.Printf("parsedHand: %v\n", parsedHand)
 }
